037958595e0fab090fd9af99d47824acb9111712
Oscar Mendez <omendez1@cobogpgp7171.global.publicisgroupe.net>
2017-11-07 17:24:43 -0500

    Feat: add event handler submit comment form

--- src/components/addComment.js
+++ src/components/addComment.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const AddComment = ( ) => {
-    return (
-        <div className="o-grid__cell">
-            <h3 className="c-heading u-super">Add comment</h3>
-            <form>
-                <label>
-                    Name
-                    <input className="c-field" name="author" type="text"/>
-                </label>
-                <br />
-                <label>
-                    Comment:
-                    <textarea className="c-field" name="numberOfGuests" ></textarea>
-                </label>
-                <button type="button" className="c-button c-button--success">Submit</button>
+class AddComment extends Component {
 
-            </form>
-        </div>
-    )
+    handleSubmit = (event) => {
+        console.log(event);
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <div className="o-grid__cell">
+                <h3 className="c-heading u-super">Add comment</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name
+                        <input className="c-field" name="author" type="text"/>
+                    </label>
+                    <br />
+                    <label>
+                        Comment:
+                        <textarea className="c-field" name="numberOfGuests" ></textarea>
+                    </label>
+                    <button type="button" className="c-button c-button--success">Submit</button>
+                </form>
+            </div>
+        )
+    }
 };
 
 export default AddComment
